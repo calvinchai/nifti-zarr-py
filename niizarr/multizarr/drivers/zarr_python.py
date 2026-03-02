eaea11b3a3bfaa90292e9a8e0d43a9f4524cec8a
Kaidong Chai <dev@kchai.me>
2026-03-02 13:43:18 -0500

    refactor: streamline imports in zarr_python.py for improved clarity
    - Removed conditional type checking for imports, simplifying the import statements.
    - Enhanced readability by directly importing necessary components from the zarr library.

--- niizarr/multizarr/drivers/zarr_python.py
+++ niizarr/multizarr/drivers/zarr_python.py
@@ -22,10 +22,9 @@ from ..abc import ZarrArray, ZarrArrayConfig, ZarrGroup
 from ..helpers import _compute_zarr_layout
 from ..zarr_config import ZarrConfig
 
-if TYPE_CHECKING:
-    from zarr.core.array import CompressorsLike
-    from zarr.core.chunk_key_encodings import ChunkKeyEncodingLike, ChunkKeyEncodingParams
-    from .._typing import Unpack
+from zarr.core.array import CompressorsLike
+from zarr.core.chunk_key_encodings import ChunkKeyEncodingLike, ChunkKeyEncodingParams
+from .._typing import Unpack
 
 class ZarrPythonArray(ZarrArray):
     """Zarr Array implementation using the zarr-python library."""
